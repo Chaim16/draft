38edf212683ac61a10fabf1b61e0f2620d6182e5
chaim16 <yqm.0229@qq.com>
2025-02-10 01:21:14 +0800

    feat: 用户注册

--- market/views/user_view.py
+++ market/views/user_view.py
@@ -26,21 +26,17 @@ class UserViewSet(viewsets.ViewSet):
         serializer = RegisterSerializer(data=request.data)
         if not serializer.is_valid():
             raise ParamsException(str(serializer.errors))
-        try:
-            params = json.loads(request.body)
-            username = params.get('username')
-            password1 = params.get('password1')
-            password2 = params.get('password2')
-            gander = params.get('gander')
-            phone = params.get('phone')
-            nickname = params.get('nickname')
+        params = json.loads(request.body)
+        username = params.get('username')
+        password1 = params.get('password1')
+        password2 = params.get('password2')
+        gander = params.get('gander')
+        phone = params.get('phone')
+        nickname = params.get('nickname')
 
-            if password1 != password2:
-                raise BusinessException("两次密码不一致")
+        if password1 != password2:
+            raise BusinessException("两次密码不一致")
 
-            user_model = UserModel()
-            user_model.register(username, password1, nickname, gander, phone)
-            return setResult()
-        except Exception as e:
-            logger.error("注册用户失败，{}".format(traceback.format_exc()))
-            raise BusinessException("注册用户失败")
+        user_model = UserModel()
+        user_model.register(username, password1, nickname, gander, phone)
+        return setResult()
